5fe1b6fd2993137ae68343a720cb6992f8035a22
mansour <amaouche.mansour2002@gmail.com>
2021-12-27 20:40:18 +0100

    initial commit

---
+++ list1.py
@@ -0,0 +1,5 @@
+numbers = [] 
+
+for i in range(0, 300):  # go through all numbers 0-10 
+    if i % 2 == 0:  # check if number is divisible by 2 
+        numbers.append(i)  # if so, add to the list 
