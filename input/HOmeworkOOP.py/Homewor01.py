0288e04cdf9aaf01f5855ae0ab66336285baa5fc
chahrr <96632761+chahrr@users.noreply.github.com>
2021-12-28 19:15:12 +0100

    Add files via upload

--- input/HOmeworkOOP.py/Homewor01.py
+++ input/HOmeworkOOP.py/Homewor01.py
@@ -6,5 +6,5 @@ class Rectangle():
     def rectangle_area(self):
         return self.length*self.width
 
-newRectangle = Rectangle(15, 10)
+newRectangle = Rectangle(12, 10)
 print(newRectangle.rectangle_area())
